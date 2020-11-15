0cdb2fa558c59ebc54925afd1f96e8044d6e84e2
plony benav <menachemfuterfas@gmail.com>
2020-11-15 01:45:40 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,17 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
+  def key_for_min_value(name_hash)
 
-end
+    minumem_value = 1000000000
+    the_key = ""
+    name_hash.each do |key, value|
+      if value < minumem_value
+        minumem_value = value
+        the_key = key
+      end
+    end
+    return nil if the_key == ""
+     the_key
+     
+  end
