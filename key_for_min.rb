f886cad6c644c5adef1921a2201d8fe4713cccc4
plony benav <menachemfuterfas@gmail.com>
2020-11-15 01:36:38 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
   def key_for_min_value(name_hash)
-    if name_hash == {}
+    if name_hash[0] == nil
       return false
     end
     minumem_value = 1000000000
