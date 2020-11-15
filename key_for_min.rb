46e9b7f91c89fc59e2650b255dd19482eb90d8db
plony benav <menachemfuterfas@gmail.com>
2020-11-15 01:45:30 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,10 +3,10 @@
 
   def key_for_min_value(name_hash)
 
-    minumem_value = ""
+    minumem_value = 1000000000
     the_key = ""
     name_hash.each do |key, value|
-      if value < minumem_value && != 0
+      if value < minumem_value
         minumem_value = value
         the_key = key
       end
